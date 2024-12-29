84cae3c8b89d4af7767d3a0cbb7183849678d930
Edoardo C <edocerioni@outlook.it>
2024-12-29 20:04:04 +0100

    migliorie gioco Sss Crt Frbc

--- sassoCartaForbice.py
+++ sassoCartaForbice.py
@@ -1,5 +1,6 @@
 import random
 numero_casuale = random.randint(1, 3)
+print(numero_casuale)
 
 if numero_casuale == 1:
     print("il tuo avversario ha scelto sasso\n")
@@ -11,8 +12,11 @@ else:
 print("Sfida all'ultimo byte contro Mr.Python a sasso carta forbice!!!\n\n")
 
 nome_giocatore = input("inserisci nome giocatore: ")
-
 scelta_giocatore= input(f"scegli la tua mossa {nome_giocatore} (sasso, carta, forbice): ")
+
+while scelta_giocatore != "sasso" and scelta_giocatore != "carta" and scelta_giocatore != "forbice":
+    print("Mossa non riconosciuta!")
+    scelta_giocatore = input(f"Scegli una mossa valida {nome_giocatore} (sasso, carta, forbice): ")
 
 if scelta_giocatore == "sasso":
     scelta_giocatore = 1
@@ -21,16 +25,31 @@ elif scelta_giocatore == "carta":
 elif scelta_giocatore == "forbice":
     scelta_giocatore = 3
 
+while scelta_giocatore == numero_casuale:
+    print(f"{nome_giocatore}, hai pareggiato con Mr. Python!")
+    numero_casuale = random.randint(1, 3)
+    print(numero_casuale)
+    scelta_giocatore = input(f'{nome_giocatore} scegli una nuova mossa: ')
 
-if scelta_giocatore == numero_casuale:
-    print("Bin bum ba le... giu!!!")
-    print(f"{nome_giocatore} hai pareggiato con MrPython")
-elif scelta_giocatore != numero_casuale:
-    print("Bin bum ba le... giu!!!")
-    if [scelta_giocatore == 2 and numero_casuale == 1] or [scelta_giocatore == 3 and numero_casuale == 2] or[scelta_giocatore == 1 and numero_casuale == 3]:
-        print(f"{nome_giocatore} hai vinto!!")
-    else:
-        print(f"Mi dispiace {nome_giocatore} ha vinto MrPython")
+if (scelta_giocatore == 1 and numero_casuale == 3) or (scelta_giocatore == 2 and numero_casuale == 1) or (scelta_giocatore == 3 and numero_casuale == 2):
+    print(f"{nome_giocatore} hai vinto!!")
+
+else:
+    print(f"Mi dispiace {nome_giocatore}, ha vinto Mr. Python!")
+
+"""risposta_giocatore = input(f"{nome_giocatore} Vuoi rigiocare? (si/no): ")
+
+if risposta_giocatore == "si":
+    numero_casuale = random.randint(1, 3)
+    print(numero_casuale)
+    scelta_giocatore = input(f'{nome_giocatore} scegli una nuova mossa: ')
+else:
+    print("Grazie di aver giocato!")"""
+
+
+
+
+
 
 
 
